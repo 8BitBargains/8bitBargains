996ad574c920c1ceb013e6cba8289f027fc62086
Don Leistman <donleistman@gmail.com>
2018-04-17 15:49:46 -0400

    defined games model and initial game routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/games', require('./games'))
+router.use('/orders', require('./orders'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
