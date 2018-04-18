a6a339e02f8953d4c0068e60f230866a5fc06d4b
Don Leistman <donleistman@gmail.com>
2018-04-18 10:39:01 -0400

    added a bit of semantic UI to browse view

--- client/components/Browse.js
+++ client/components/Browse.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react';
 
 export default class Browse extends React.Component {
   componentDidMount() {
@@ -7,18 +8,30 @@ export default class Browse extends React.Component {
 
   render() {
     return (
-      <div>
-        <ul>
-          {this.props.products && this.props.products.map(product => (
-            <li key={product.id}>
-              <a href={`/games/${product.id}`}>{product.name}</a>
-              <p>Price: {product.price}</p>
-              <p>Description: {product.description}</p>
-              <img src={product.coverUrl} />
-            </li>
-          ))}
-        </ul>
-      </div>
+      // <div>
+      //   <ul>
+      //     {this.props.products && this.props.products.map(product => (
+      //       <li key={product.id}>
+      //         <a href={`/games/${product.id}`}>{product.name}</a>
+      //         <p>Price: {product.price}</p>
+      //         <p>Description: {product.description}</p>
+      //         <img src={product.coverUrl} />
+      //       </li>
+      //     ))}
+      //   </ul>
+      //   </div>
+      <Card.Group>
+        {this.props.products && this.props.products.map(product => (
+          <Card key={product.id}>
+            <Image src={product.coverUrl} />
+            <Card.Content>
+              <Card.Header>{product.title}</Card.Header>
+              <Card.Meta>Price: {product.price}</Card.Meta>
+              <Card.Description>{product.description}</Card.Description>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
     )
   }
 }
