f2871d07d0b2053c7b5be958dccecda54d66c6df
Alex Sobiloff <asobiloff@gmail.com>
2018-04-21 15:25:28 -0400

    ADD Billing address and handleSubmit to OrderInfo Component

--- client/components/OrderInfo.js
+++ client/components/OrderInfo.js
@@ -2,32 +2,74 @@ import React, { Component } from 'react';
 import { Checkbox, Input, Container, Button, Form, Select } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { shippingOptions, stateOptions, countryOptions } from '../utils';
+import { submitInfo } from '../store';
 
-const Shipping = () => {
+const Shipping = (props) => {
+
+  const { handleSubmit } = props.handleSubmit
 
   return (
-    <Form>
-      <Form.Field>
-        <label> Address </label>
-        <Input placeholder='123 Abc St.' />
-      </Form.Field>
-      <Form.Field>
-        <label> City</label>
-        <Input placeholder='Your City' />
-      </Form.Field>
-      <Form.Field>
-        <label> State</label>
-        <Select placeholder='Select your State' options={stateOptions} />
-      </Form.Field>
-      <Form.Field>
-        <Select placeholder='Select your country' options={countryOptions} />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label='I agree to pay for this stuff' />
-      </Form.Field>
-      <Select placeholder='Shipping Options' options={shippingOptions} />
-      <Button type='submit'>Submit</Button>
-    </Form>
+    <Container>
+      <Form id="order-info-form" >
+        <div className="component" >
+          <div className="section">
+            <label>Shipping Address</label>
+            <Form.Field required>
+              <label>Name</label>
+              <Input placeholder="Name" />
+            </Form.Field>
+            <Form.Field required>
+              <label>Address</label>
+              <Input placeholder="Address" />
+            </Form.Field>
+            <Form.Field required>
+              <label>City</label>
+              <Input placeholder="City" />
+            </Form.Field>
+            <Form.Field required>
+              <label>State</label>
+              <Select placeholder="State" options={stateOptions} />
+            </Form.Field>
+            <Form.Field required>
+              <label>Country</label>
+              <Select placeholder="Country" options={countryOptions} />
+            </Form.Field>
+            <Form.Field required>
+              <label>Shipping Options</label>
+              <Select placeholder="Shipping Options" options={shippingOptions} />
+            </Form.Field>
+          </div>
+          <div className="section">
+            <label>Billing Address</label>
+            <Checkbox label="Same as Shipping Address" />
+            <Form.Field>
+              <label>Name</label>
+              <Input placeholder="Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Address</label>
+              <Input placeholder="Address" />
+            </Form.Field>
+            <Form.Field>
+              <label>City</label>
+              <Input placeholder="City" />
+            </Form.Field>
+            <Form.Field>
+              <label>State</label>
+              <Select placeholder="State" options={stateOptions} />
+            </Form.Field>
+            <Form.Field>
+              <label>Country</label>
+              <Select placeholder="Country" options={countryOptions} />
+            </Form.Field>
+          </div>
+        </div>
+        <Form.Field>
+          <Checkbox label="I agree to pay for this stuff" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
   );
 };
 
@@ -36,7 +78,7 @@ class OrderInfo extends Component {
   render(){
     return (
       <Container>
-        <Shipping />
+        <Shipping handleChange={this.props.handleSubmit}/>
       </Container>
     );
   }
@@ -47,13 +89,10 @@ const mapState = () => {
   };
 };
 
-const mapDispatch = () => {
+const mapDispatch = dispatch => {
   return {
-    loadCart: () => {
-
-    },
-    handleChange: () => {
-
+    handleSubmit: (formData) => {
+      dispatch(submitInfo(formData));
     }
   };
 };
