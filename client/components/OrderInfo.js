9468aff462267b81516f2e88d41915d464cd7db2
Alex Sobiloff <asobiloff@gmail.com>
2018-04-21 20:58:55 -0400

    FINISH shipping form and address dispatch

--- client/components/OrderInfo.js
+++ client/components/OrderInfo.js
@@ -4,101 +4,120 @@ import { connect } from 'react-redux';
 import { shippingOptions, stateOptions, countryOptions } from '../utils';
 import { updateAddress } from '../store';
 
-const Shipping = (props) => {
-  console.log('shipping props ', props)
-  const { handleSubmit } = props.handleSubmit;
-
-  return (
-    <Container>
-      <Form id="order-info-form" onSubmit={ (event, info) => handleSubmit(info)} >
-        <div className="component" >
-          <div className="section">
-            <label>Shipping Address</label>
-            <Form.Field required>
-              <label>Name</label>
-              <Input placeholder="Name" />
-            </Form.Field>
-            <Form.Field required>
-              <label>Address</label>
-              <Input placeholder="Address" />
-            </Form.Field>
-            <Form.Field required>
-              <label>City</label>
-              <Input placeholder="City" />
-            </Form.Field>
-            <Form.Field required>
-              <label>State</label>
-              <Select placeholder="State" options={stateOptions} />
-            </Form.Field>
-            <Form.Field required>
-              <label>Country</label>
-              <Select placeholder="Country" options={countryOptions} />
-            </Form.Field>
-            <Form.Field required>
-              <label>Shipping Options</label>
-              <Select placeholder="Shipping Options" options={shippingOptions} />
-            </Form.Field>
-          </div>
-          {/* <div className="section">
-            <label>Billing Address</label>
-            <Checkbox label="Same as Shipping Address" />
-            <Form.Field>
-              <label>Name</label>
-              <Input placeholder="Name" />
-            </Form.Field>
-            <Form.Field>
-              <label>Address</label>
-              <Input placeholder="Address" />
-            </Form.Field>
-            <Form.Field>
-              <label>City</label>
-              <Input placeholder="City" />
-            </Form.Field>
-            <Form.Field>
-              <label>State</label>
-              <Select placeholder="State" options={stateOptions} />
-            </Form.Field>
-            <Form.Field>
-              <label>Country</label>
-              <Select placeholder="Country" options={countryOptions} />
-            </Form.Field>
-          </div> */}
-        </div>
-        <Form.Field>
-          <Checkbox label="I agree to pay for this stuff" />
-        </Form.Field>
-        <Button type="submit">Submit</Button>
-      </Form>
-    </Container>
-  );
+const initialState = {
+  name: '',
+  address: '',
+  city: '',
+  state: '',
+  country: '',
+  shipping: '',
 };
-
 class OrderInfo extends Component {
+  constructor(props){
+    super(props);
+    this.state = initialState;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (evt, value) {
+    // updates local state only based on the change of the form inputs
+    let newState = {};
+    let key;
+
+    if (typeof value === 'string'){
+      // hack to get around the weird way react semantic ui handles select elements
+      key = evt;
+    } else {
+      key = value.name;
+      value = value.value;
+    }
+    newState[key] = value;
+    this.setState(newState);
+  }
+
+  handleSubmit (formData) {
+    // takes the submit information from the form and passes it onto the container
+    const { name, address, city, state, country } = formData;
+    const shippingAddress = `${name},${address},${city},${state},${country}`;
+    console.log('address: ', shippingAddress)
+    this.props.handleSubmit(shippingAddress);
+    this.setState(initialState);
+  }
   render(){
-    console.log('order info props ', this.props)
+    const { handleSubmit, handleChange } = this;
+    const { name, address, city, state, country, shipping } = this.state;
+
     return (
       <Container>
-        <Shipping handleSubmit={this.props.handleSubmit}/>
+        <Form id="order-info-form" onSubmit={ () => handleSubmit(this.state)} >
+          <div className="component" >
+            <div className="section">
+              <label>Shipping Address</label>
+              <Form.Field required>
+                <label>Name</label>
+                <Input name="name" value={name} onChange={handleChange} placeholder="Name" />
+              </Form.Field>
+              <Form.Field required>
+                <label>Address</label>
+                <Input name="address" value={address} onChange={handleChange} placeholder="Address" />
+              </Form.Field>
+              <Form.Field required>
+                <label>City</label>
+                <Input name="city" value={city} onChange={handleChange} placeholder="City" />
+              </Form.Field>
+              <Form.Field required>
+                <label>State</label>
+                <Select
+                  name="state"
+                  value={state}
+                  onChange={(evt, { value }) => handleChange('state', value)}
+                  placeholder="State"
+                  options={stateOptions} />
+              </Form.Field>
+              <Form.Field required>
+                <label>Country</label>
+                <Select
+                  name="country"
+                  value={country}
+                  onChange={(evt, { value }) => handleChange('country', value)}
+                  placeholder="Country"
+                  options={countryOptions} />
+              </Form.Field>
+              <Form.Field required>
+                <label>Shipping Options</label>
+                <Select
+                  name="shipping"
+                  value={shipping}
+                  onChange={(evt, { value }) => handleChange('shipping', value)}
+                  placeholder="Shipping Options"
+                  options={shippingOptions} />
+              </Form.Field>
+            </div>
+            <div className="section">
+              <label>Billing Address</label>
+              <Checkbox label="Same as Shipping Address" />
+            </div>
+          </div>
+          <Form.Field>
+            <Checkbox label="I agree to pay for this stuff" />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
       </Container>
     );
   }
 }
 
-const mapState = () => {
-  return {
-  };
-};
-
 const mapDispatch = dispatch => {
   return {
     handleSubmit: (formData) => {
       // logic for converting to string here
-      console.log('form data ', formData)
-      // dispatch(updateAddress(formData));
+      dispatch(updateAddress(formData));
     }
   };
 };
 
-const OrderInfoContainer = connect(mapState, mapDispatch)(OrderInfo);
+const OrderInfoContainer = connect(null, mapDispatch)(OrderInfo);
 
 export default OrderInfoContainer;
