9fca89f1103ef935dd73d6109be584426a4bb2a3
Mario Villacreses <MarioVillacreses@outlook.com>
2018-04-24 12:54:25 -0400

    Wrap react components on AllUsers

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -4,10 +4,12 @@ import { Button } from 'semantic-ui-react';
 const AllUsers = props => (
   <div>
     {props.users ? props.users.map(user => (
-      <p key={user.id}>User no. {user.id}, email: {user.email}</p>
-      <Button>
-        {user.isAdmin ? 'Remove' : 'Make'} Admin
-      </Button>
+      <div>
+        <p key={user.id}>User no. {user.id}, email: {user.email}</p>
+        <Button>
+          {user.isAdmin ? 'Remove' : 'Make'} Admin
+        </Button>
+      </div>
     )) : <p>No users to show!</p>}
   </div>
 )
